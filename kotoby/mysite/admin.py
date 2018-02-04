ae1bdf3807dbcd3eed2fcc840e38498c9629161c
Tarek-Essam <tarekessam1311@gmail.com>
2018-02-04 18:11:15 +0200

    update

--- kotoby/mysite/admin.py
+++ kotoby/mysite/admin.py
@@ -2,3 +2,8 @@ from django.contrib import admin
 from mysite import models
 
 admin.site.register(models.Users)
+admin.site.register(models.Authors)
+admin.site.register(models.Books)
+admin.site.register(models.Status)
+admin.site.register(models.Category)
+admin.site.register(models.Rate)
